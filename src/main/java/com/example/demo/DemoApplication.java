7a7adbe5a5ff980c2000b9f0dd76854176b0f25c
Ethan <154518484@qq.com>
2017-10-24 15:41:48 +0800

    2

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,6 +9,7 @@ public class DemoApplication {
 	public static void main(String[] args) {
 
 		System.err.println("1");
+		System.err.println("2");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
