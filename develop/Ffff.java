fb3c38bb850e661b424c1edda52220bb1f9b53af
Ethan <154518484@qq.com>
2017-10-24 15:48:26 +0800

    develop-Ffff

---
+++ develop/Ffff.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+
+		System.err.println("1");
+		System.err.println("2");
+		System.err.println("develop");
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
